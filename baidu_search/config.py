9ffc5c39e91a5356ca81e0299c706bf9f6f0d618
Haoran Lin <linhaoran917@163.com>
2016-09-23 14:46:35 +0800

    anti-anti-crawler works

--- baidu_search/config.py
+++ baidu_search/config.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
+from sqlalchemy import create_engine
+
 __author__ = 'Haoran'
+
+
+mysql_db = create_engine('mysql+pymysql://root:growth@192.168.20.96:3306/sem?charset=utf8', echo=False)
+user_agent = """Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)
+                AppleWebKit/537.36 (KHTML, like Gecko)
+                Chrome/53.0.2785.116 Safari/537.36"""
+headers = {'User-Agent': user_agent}
